b74cd36a6270d0c89c4a7136fe3d579042d5bdc9
rajeshmondal54 <rm4223536@gmail.com>
2024-06-18 05:07:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//your code here
+document.addEventListener('DOMContentLoaded', () => {
+    const input = document.getElementById('input');
+    let currentInput = '';
+
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            const id = button.id;
+            if (id === 'clr') {
+                currentInput = '';
+            } else if (id === 'ans') {
+                try {
+                    currentInput = eval(currentInput);
+                } catch (error) {
+                    currentInput = 'Error';
+                }
+            } else {
+                const value = button.innerText;
+                currentInput += value;
+            }
+            input.value = currentInput;
+        });
+    });
+});
